d0dec09a0d077d53a4302b3ec8ba74707a9f2db8
yaowenqiang <yaowenqiang111@163.com>
2026-08-20 23:31:51 +0800

    add more examples

--- snake_game/main.rs
+++ snake_game/main.rs
@@ -1,3 +1,6 @@
-fn fn main() {
-    println!("hello snake game!")
+fn main() {
+    let mut message = "Hello, world!";
+    println!("{}", message);
+    message = "Hello, Rust!";
+    println!("{}", message);
 }
